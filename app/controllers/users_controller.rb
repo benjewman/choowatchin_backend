3e5b66b487e050b22e357dc247795ac77783b529
benjewman <bennyadave@gmail.com>
2020-01-08 13:21:11 -0600

    creates user with encrypted password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,21 @@
 class UsersController < ApplicationController
+    wrap_parameters :user, include: [:full_name, :password, :username, :email]
+    def index
+        users = User.all
+        render json: users
+    end
+    
     def create
         user = User.new(user_params)
         if user.save
             puts '------------------------------------------'
             puts user
+            render json: user
         else
+            byebug
             puts '---------------------------------------'
             puts 'NOPE'
+            render json: {error: 'whoopsie'}
         end
     end 
 
