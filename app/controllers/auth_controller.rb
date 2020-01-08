ff15ab8251b587c2fe73bbd72fcddff8b9500b8b
benjewman <bennyadave@gmail.com>
2020-01-08 14:00:18 -0600

    login creates token

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,11 @@
 class AuthController < ApplicationController
     def create
-        
+        user = User.find_by(username: params['username'])
+        if user && user.authenticate(params['password'])
+            token = JWT.encode({user_id: user.id}, 'chookey', 'HS256')
+            render json: { id: user.id, username: user.username, token: token }
+        else
+            render json: { error: 'invalid credentials' }, status: 401
+        end
     end
 end
