087b59bf8cbc1e1412440df422f886d87ea64cb1
benjewman <bennyadave@gmail.com>
2020-01-08 15:36:26 -0600

    authorizes users signed in

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,9 +3,20 @@ class AuthController < ApplicationController
         user = User.find_by(username: params['username'])
         if user && user.authenticate(params['password'])
             token = JWT.encode({user_id: user.id}, 'chookey', 'HS256')
-            render json: { id: user.id, username: user.username, token: token }
+            render json: { user: user, token: token }
         else
             render json: { error: 'invalid credentials' }, status: 401
+        end
+    end
+
+    def show
+        token = request.headers['Authorization'].split(' ')[1]
+        user_id = JWT.decode(token, 'chookey', true, { algorithm: 'HS256' })[0]['user_id']
+        user = User.find(user_id)
+        if user 
+            render json: { user: user }
+        else
+            render json: { error: 'not signed in' }, status: 401
         end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :reviews
   resources :users
   post '/auth', to: 'auth#create'
+  get '/current_user', to: 'auth#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
