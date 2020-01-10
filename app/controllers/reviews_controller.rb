4a839288a13f658cee2907554429e2ea27f0c510
benjewman <bennyadave@gmail.com>
2020-01-10 00:03:08 -0600

    changed fetch back to frontend only

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,16 @@
 class ReviewsController < ApplicationController
+    def show
+        review = Review.find_by(id: params[:id])
+        show = review.show
+        # if review 
+        #     fetch("http://www.omdbapi.com/?apikey=49f89f6c&i=#{review.show.imdbID}")
+        #     .then(resp => resp.json())
+        #     # BREAKING ON NEXT LINE
+        #     .then(show => return render json: {review: review, show: show, user: review.user})
+        # else
+        #     render json: { error: 'review not found' }
+        # end
+        user = review.user
+        render json: {review: review, show: show, user: user}
+    end
 end
