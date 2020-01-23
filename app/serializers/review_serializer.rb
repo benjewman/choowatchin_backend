71ea2b4b191039d51dcb5699acdf821e4ed26990
benjewman <bennyadave@gmail.com>
2020-01-23 10:12:33 -0600

    commented reveiwserialiszer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,16 +1,16 @@
 class ReviewSerializer < ActiveModel::Serializer
     include Rails.application.routes.url_helpers
-  attributes :id, :stamp, :content, :show_id, :user, :show
+  attributes :id, :stamp, :content, :show_id, :show, #:user, 
   belongs_to :user, include_nested_attributes: :true
 
   def user
-     user = object.user.attributes
-     if object.avatar.attached?
-      user['avatar'] = url_for(object.user.avatar)
-     else 
-      user['avatar'] = object.user.pic
-     end
-     return user
+    #  user = object.user.attributes
+    #  if object.avatar.attached?
+    #   user['avatar'] = url_for(object.user.avatar)
+    #  else 
+    #   user['avatar'] = object.user.pic
+    #  end
+    #  return user
   end 
 
 end
